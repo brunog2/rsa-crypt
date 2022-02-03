5c2496e5608b13b8a3f7c50918227519218f9841
LcR4 <96593662+LcR4@users.noreply.github.com>
2022-02-03 17:08:18 -0300

    Add files via upload

---
+++ projeto.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int modInverse(int a, int m) 
+{ 
+    a = a%m; 
+    for (int x=1; x<m; x++) 
+       if ((a*x) % m == 1) 
+          return x;
+}
+
+int descriptografar(int d, int k)
+{
+    int n;
+    int resultado, pot;
+    scanf("%d", &n);
+    pot = n % d;
+    resultado = 1;
+    for( ; k > 0; k /= 2)
+    {
+        if (k % 2 == 1)
+        {
+            resultado = (resultado * pot) % d;
+        }
+            pot = (pot * pot) % d;
+        
+    }
+    return resultado;
+}
+
+int main()
+{
+    int k, d;
+    int p, q, e;
+    int res;
+    char alfabeto[28] = " abcdefghijklmnopqrstuvwxyz ";
+    scanf("%d %d", &p, &q);
+    scanf("%d", &e);
+    d = p*q;
+    k = modInverse(e, ((p-1)*(q-1)));
+    for(int i = 1; i < 7; i++)
+    {
+        res = descriptografar(d, k);
+        printf("%c", alfabeto[res-1]);
+    }
+    return 0;
+}
