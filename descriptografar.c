76fc59cb2126757c9029c3aee18a3e4df0d0bd5f
LcR4 <96593662+LcR4@users.noreply.github.com>
2022-02-03 17:32:10 -0300

    Update descriptografar.c

--- descriptografar.c
+++ descriptografar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int modInverse(int a, int m) 
+int modInverse(int a, int m) //Função para encontrar o segundo numero da Chave Privada(inverso modular).
 { 
     a = a%m; 
     for (int x=1; x<m; x++) 
@@ -8,7 +8,7 @@ int modInverse(int a, int m)
           return x;
 }
 
-int descriptografar(int d, int k)
+int descriptografar(int d, int k) //Função que recebe a Chave Privada e o Codigo criptografado e descriptografa.
 {
     int n;
     int resultado, pot;
@@ -32,16 +32,19 @@ int main()
     int k, d;
     int p, q, e;
     int res;
-    char alfabeto[28] = " abcdefghijklmnopqrstuvwxyz ";
+    char alfabeto[28] = " abcdefghijklmnopqrstuvwxyz "; //Alfabeto que o professor pediu. A = 2, B = 3... Z = 27, espaço = 28.
     scanf("%d %d", &p, &q);
     scanf("%d", &e);
-    d = p*q;
-    k = modInverse(e, ((p-1)*(q-1)));
-    for(int i = 1; i < 50; i++)
+    d = p*q; // d = primeiro numero da chave privada.
+    k = modInverse(e, ((p-1)*(q-1))); // k = segundo numero da chave privada.
+    for(int i = 1; i < 50; i++)//for final pra receber e descriptografar todos os numeros.
     {
         res = descriptografar(d, k);
-        printf("%c", alfabeto[res-1]);
+        printf("%c", alfabeto[res-1]); //recebe o numero inteiro da função, e busca no alfabeto da linha 35 o numero correspondente.
     }
     printf("\n");
     return 0;
 }
+//formato de entrada por arquivo:
+// p q e
+// codigo criptografado
