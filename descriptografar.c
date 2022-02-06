be75da2e391e70e0c87349fd5bfbf1e206acea7e
LcR4 <96593662+LcR4@users.noreply.github.com>
2022-02-06 12:38:26 -0300

    Update descriptografar.c

--- descriptografar.c
+++ descriptografar.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int modInverse(int a, int m) //Função para encontrar o segundo numero da Chave Privada(inverso modular).
+long long int modInverse(long long int a, long long int m) //Função para encontrar o segundo numero da Chave Privada(inverso modular).
 { 
     a = a%m; 
-    for (int x=1; x<m; x++) 
+    for (long long int x=1; x<m; x++) 
        if ((a*x) % m == 1) 
           return x;
 }
 
-int descriptografar(int d, int k, int i, int codigo[]) //Função que recebe a Chave Privada e o Codigo criptografado e descriptografa.
+long long int descriptografar(long long int d, long long int k, int i, long long int codigo[]) //Função que recebe a Chave Privada e o Codigo criptografado e descriptografa.
 {
-    int n;
-    int resultado, pot;
+    long long int n;
+    long long int resultado, pot;
     n = codigo[i];
     pot = n % d;
     resultado = 1;
@@ -29,16 +29,16 @@ int descriptografar(int d, int k, int i, int codigo[]) //Função que recebe a C
 
 int main()
 {
-    int k, d;
-    int p, q, e;
-    int res;
+    long long int k, d;
+    long long int p, q, e;
+    long long int res;
     int aux;
     int n = 0;
     char alfabeto[28] = " abcdefghijklmnopqrstuvwxyz "; //Alfabeto que o professor pediu. A = 2, B = 3... Z = 27, espaço = 28.
-    scanf("%d %d", &p, &q);
-    scanf("%d", &e);
+    scanf("%lld %lld", &p, &q);
+    scanf("%lld", &e);
     
-    int codigo[2000];
+    long long int codigo[2000];
 
     for(aux = 0; aux < 2000; aux++) // for para definir todos os elementos do array codigo como -555.
     {
@@ -47,7 +47,7 @@ int main()
 
     for(aux = 0; aux < 2000; aux++)  // for para substituir os elementos do array codigo pelo codigo criptografado.
     {
-        if(scanf("%d", &codigo[aux])==EOF){aux+=2000;}
+        if(scanf("%lld", &codigo[aux])==EOF){aux+=2000;}
     }
 
     for(aux = 0; aux != 2000; aux++) // for para contar quantos elementos o array tem até encontrar o primeiro -555.
